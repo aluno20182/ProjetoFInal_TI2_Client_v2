7cb6f8891739d55b9d2b713c872b18435980a4a9
aluno20182 <47854257+aluno20182@users.noreply.github.com>
2019-04-15 23:44:23 +0100

    CSS
    Adição do Backgroud

--- src/App.js
+++ src/App.js
@@ -7,31 +7,36 @@ import Footer from "./Components/Footer.js";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <h1>Trabalho Final</h1>
-      <h2>Tomablr</h2>
-      <div class="custom-select">
-        <select>
-          <option value="0">Post Por Pagina:</option>
-          <option value="1">5</option>
-          <option value="2">10</option>
-          <option value="3">15</option>
-          <option value="4">20</option>
-          <option value="5">25</option>
-          <option value="6">40</option>
-        </select>
+    <div class="container demo">
+      <div class="content">
+        <div id="large-header" class="large-header">
+          <canvas id="demo-canvas" />
+          <Header />
+          <h1>Trabalho Final</h1>
+          <h2>Tomablr</h2>
+          <div class="custom-select">
+            <select>
+              <option value="0">Post Por Pagina:</option>
+              <option value="1">5</option>
+              <option value="2">10</option>
+              <option value="3">15</option>
+              <option value="4">20</option>
+              <option value="5">25</option>
+              <option value="6">40</option>
+            </select>
+          </div>
+          <br />
+          <MainContent />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <Footer />
+        </div>
       </div>
-      <br />
-      <MainContent />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Footer />
     </div>
   );
 }
--- src/Components/Moldura.js
+++ src/Components/Moldura.js
@@ -2,18 +2,30 @@ import React from "react";
 
 function Moldura() {
   return (
-    <div class="foto">
+    <div >
       <img
+      class="moldura"
+        id="foto"
         src="https://images.unsplash.com/photo-1518098268026-4e89f1a2cd8e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-        alt="foto 2"
+        alt="foto 1"
       />
+      <br />
+      <br />
+      <br />
+      <br />
 
-      <img
+      <img class="moldura"
+        id="foto"
         src="http://info.annimaddaloni.de/wp-content/uploads/2018/10/Lake_Mountains_Winter_438866.jpg"
         alt="foto 2"
       />
+      <br />
+      <br />
+      <br />
+      <br />
 
-      <img
+      <img class="moldura"
+        id="foto"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_UVkBN5Wr0cZI6NtQZTWDIfV-DL8WWUkSs1YnLPJY3NSVixBw"
         alt="foto 3"
       />
