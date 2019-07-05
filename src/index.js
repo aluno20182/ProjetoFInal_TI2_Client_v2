5fc8ae68048516e5ee20b7f1bd30819b66138215
Ricardo Valente Pinheiro <aluno20182@ipt.pt>
2019-07-05 22:50:51 +0100

    ACEDER AO LOGIN (TESTE)

--- src/index.js
+++ src/index.js
@@ -6,3 +6,7 @@ import "./styles.css";
 import { App } from "./App.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+/*import { Login } from "./Components/Login.js";
+
+ReactDOM.render(<Login />, document.getElementById("root"));*/
